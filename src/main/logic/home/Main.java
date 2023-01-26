57b6d580b3ce762de5b3b272a8eee7f9c8499f23
colale <colale1998@gmai.com>
2023-01-26 02:35:07 +0100

    start random generation

--- src/main/logic/home/Main.java
+++ src/main/logic/home/Main.java
@@ -3,10 +3,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import java.io.FileOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.Random;
 
 public class Main extends Application {
     @Override
@@ -19,9 +20,14 @@ public class Main extends Application {
         stage.show();
     }
     public static void main(String[] args) throws IOException {
+        Main program = new Main();
+        program.chooseConfiguration();
+    }
+        public void chooseConfiguration() throws IOException {
         FileInputStream propsInput = new FileInputStream("src/main/logic/resources/config.properties");
         Properties prop = new Properties();
         prop.load(propsInput);
+        this.RandomConfigGenerate(prop);
         String CLI_UI = prop.getProperty("CLI_UI");
         String USER_DAO_ON_FILESYSTEM=prop.getProperty("USER_DAO_ON_FILESYSTEM");
         if (CLI_UI.equals("false")){
@@ -31,4 +37,16 @@ public class Main extends Application {
             propsInput.close();
             CLIHomeController c=new CLIHomeController();}
         }
+
+public void RandomConfigGenerate(Properties prop) throws IOException {
+        FileOutputStream propsOutput = new FileOutputStream("src/main/logic/resources/config.properties");
+        Random random = new Random();
+        String value;
+        value=(random.nextInt(2)==1)?"true":"false";
+        prop.setProperty("CLI_UI",value);
+        value=(random.nextInt(2)==1)?"true":"false";
+        prop.setProperty("USER_DAO_ON_FILESYSTEM",value);
+        prop.store(propsOutput, null);
+        propsOutput.close();
+}
 }
