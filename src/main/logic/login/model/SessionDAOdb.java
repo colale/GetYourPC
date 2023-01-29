6605a3eeda33c97694f538d5ae400c1c17079a6e
colale <colale1998@gmai.com>
2023-01-29 04:57:20 +0100

    login

--- src/main/logic/login/model/SessionDAOdb.java
+++ src/main/logic/login/model/SessionDAOdb.java
@@ -1,4 +1,35 @@
 package login.model;
 
+import login.bean.CredentialsInput;
+import login.bean.UserDataBean;
+
 public class SessionDAOdb {
+    private Connection conn;
+    public SessionDAOdb(Connection conn) {
+        this.conn = conn;
+    }
+
+    public UserDataBean login(CredentialsInput input) {
+        UserDataBean user = null;
+        try {
+            String sql = "SELECT * FROM users WHERE email = ? AND password = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, input.getEmail());
+            stmt.setString(2, input.getPassword());
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                user = new UserBean();
+                user.setId(rs.getInt("id"));
+                user.setEmail(rs.getString("email"));
+                user.setName(rs.getString("name"));
+                user.setLastname(rs.getString("lastname"));
+                user.setActive(rs.getInt("active"));
+            }
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
 }
