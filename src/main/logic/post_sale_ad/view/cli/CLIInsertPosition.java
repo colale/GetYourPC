7593a62348ba4355780a17c15746f674b17dee65
colale <colale1998@gmai.com>
2023-02-06 18:39:20 +0100

    postSaleAd

--- src/main/logic/post_sale_ad/view/cli/CLIInsertPosition.java
+++ src/main/logic/post_sale_ad/view/cli/CLIInsertPosition.java
@@ -1,8 +1,8 @@
 package post_sale_ad.view.cli;
 
+import exception.GeocodingException;
 import exception.SyntaxBeanException;
 import post_sale_ad.app_controller.PostSaleAdController;
-import post_sale_ad.bean.PhotoBean;
 import post_sale_ad.bean.UserGeoRequestBean;
 import post_sale_ad.bean.UserGeoResponseBean;
 
@@ -11,6 +11,7 @@ import java.util.Scanner;
 public class CLIInsertPosition {
     PostSaleAdController controller;
     UserGeoRequestBean userGeoRequestBean;
+    UserGeoResponseBean userGeoResponseBean;
 
     public CLIInsertPosition(PostSaleAdController controller) {
         this.controller = controller;
@@ -29,12 +30,29 @@ public class CLIInsertPosition {
             userGeoRequestBean.setCity(scanner.nextLine());
             System.out.println("Insert address:");
             userGeoRequestBean.setAddress(scanner.nextLine());
-        } catch (SyntaxBeanException ex) {
-            System.out.println("Error, please try again");
+            UserGeoResponseBean response = controller.searchPosition(userGeoRequestBean);
+            this.userGeoResponseBean = response;
+            System.out.println("Position: " + response.getFullAddress());
+            if (confirmRequest()) {
+                controller.publishPost();
+                System.out.println("Your post has been published! Back to home");
+            } else {
+                this.execute();
+            }
+        } catch (SyntaxBeanException syntaxException) {
+            System.out.println("Invalid data, try again");
+            this.execute();
+        } catch (GeocodingException geoEx) {
+            System.out.println("Position not found, try again");
             this.execute();
         }
+    }
 
+
+    public boolean confirmRequest() {
+        System.out.println("Is it correct? Enter 'y' for yes, or any other key for no:");
+        Scanner scanner = new Scanner(System.in);
+        if(scanner.nextLine().equals("y")){return true;}
+        else{return false;}
     }
 }
-
-
